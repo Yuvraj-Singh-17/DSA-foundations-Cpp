a9e266e3b9620b2fd19fb150eb3a8aec9ec8d6c4
Yuvraj-Singh-17 <Yuvrajsinghsolankiq@gmail.com>
2026-06-16 17:35:45 +0000

    Added print only divided by 4

---
+++ Basics/16_print_only_divisible_by_4.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std ;
+int main()
+{
+    int n ;
+    cout << "Put your no. : " ;
+    cin >> n ;
+    for (int i=0 ; i<=n ; i=i+4)
+    cout << i <<"\n" ;
+    return 0 ;
+}
